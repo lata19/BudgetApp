44eaccf8fa4d592278dc51d71f68364ef1b5c694
Nikola <nikolalatincic19@gmail.com>
2023-06-09 21:04:06 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from typing import Optional, Tuple, Union
+import customtkinter as ctk
+import datetime as dt
+
+
+class BudgetApp(ctk.CTk):
+    pass
+
+
+if __name__ == "__main__":
+    app = BudgetApp()
+    app.mainloop()
